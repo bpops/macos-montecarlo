447ce727a375db4094e4eff6c43926d55f7c79b0
Brent <bpops@users.noreply.github.com>
2022-08-04 20:29:30 -0500

    removed some commented garbage

--- macos-montecarlo.sh
+++ macos-montecarlo.sh
@@ -180,17 +180,6 @@ function install_gate(){
     echo "Installing GATE..."
 }
 
-
-#echo "Press any key to continue"
-#while [ true ] ; do
-#read -t 3 -n 1
-#if [ $? = 0 ] ; then
-#exit ;
-#else
-#echo "waiting for the keypress"
-#fi
-#done
-
 # procedure
 welcome
 set_variables

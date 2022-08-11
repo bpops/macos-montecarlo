ddc391583f8bf57c2242844db3b6bd9a94826289
Brent <bpops@users.noreply.github.com>
2022-08-10 19:10:39 -0500

    logo changes

--- macos-montecarlo.sh
+++ macos-montecarlo.sh
@@ -1,13 +1,14 @@
 #
-#                                             _ \   ___|
-#                       __ `__ \   _` |  __| |   |\___ \
-#                       |   |   | (   | (    |   |      |
-#                      _|  _|  _|\__,_|\___|\___/ _____/
-#
-#         \  |  _ \   \  |__ __| ____|   ___|    \     _ \  |      _ \
-#        |\/ | |   |   \ |   |   __|    |       _ \   |   | |     |   |
-#        |   | |   | |\  |   |   |      |      ___ \  __ <  |     |   |
-#       _|  _|\___/ _| \_|  _|  _____| \____|_/    _\_| \_\_____|\___/
+#                                            ___  ____
+#                      _ __ ___   __ _  ___ / _ \/ ___|
+#                     | '_ ` _ \ / _` |/ __| | | \___ \
+#                     | | | | | | (_| | (__| |_| |___) |
+#                     |_| |_| |_|\__,_|\___|\___/|____/
+#       __  __  ___  _   _ _____ _____    ____    _    ____  _     ___
+#      |  \/  |/ _ \| \ | |_   _| ____|  / ___|  / \  |  _ \| |   / _ \
+#      | |\/| | | | |  \| | | | |  _|   | |     / _ \ | |_) | |  | | | |
+#      | |  | | |_| | |\  | | | | |___  | |___ / ___ \|  _ <| |__| |_| |
+#      |_|  |_|\___/|_| \_| |_| |_____|  \____/_/   \_\_| \_\_____\___/
 #
 #            push-button installation of Geant4/Root/GATE on macOS
 #
@@ -52,21 +53,21 @@ function press_key(){
 function welcome(){
     echo "";
     echo "";
-    echo "                                           _ \x5C   ___|";
-    echo "                     __ \x60__ \x5C   _\x60 |  __| |   |\x5C___ \x5C";
-    echo "                     |   |   | (   | (    |   |      |";
-    echo "                    _|  _|  _|\x5C__,_|\x5C___|\x5C___/ _____/";
-    echo "";
-    echo "       \x5C  |  _ \x5C   \x5C  |__ __| ____|   ___|    \x5C     _ \x5C  |      _ \x5C";
-    echo "      |\x5C/ | |   |   \x5C |   |   __|    |       _ \x5C   |   | |     |   |";
-    echo "      |   | |   | |\x5C  |   |   |      |      ___ \x5C  __ <  |     |   |";
-    echo "     _|  _|\x5C___/ _| \x5C_|  _|  _____| \x5C____|_/    _\x5C_| \x5C_\x5C_____|\x5C___/";
+    echo "                                           ___  ____";
+    echo "                     _ __ ___   __ _  ___ / _ \x5C/ ___|";
+    echo "                    | '_ \x60 _ \x5C / _\x60 |/ __| | | \x5C___ \x5C";
+    echo "                    | | | | | | (_| | (__| |_| |___) |";
+    echo "                    |_| |_| |_|\x5C__,_|\x5C___|\x5C___/|____/";
+    echo "      __  __  ___  _   _ _____ _____    ____    _    ____  _     ___";
+    echo "     |  \x5C/  |/ _ \x5C| \x5C | |_   _| ____|  / ___|  / \x5C  |  _ \x5C| |   / _ \x5C";
+    echo "     | |\x5C/| | | | |  \x5C| | | | |  _|   | |     / _ \x5C | |_) | |  | | | |";
+    echo "     | |  | | |_| | |\x5C  | | | | |___  | |___ / ___ \x5C|  _ <| |__| |_| |";
+    echo "     |_|  |_|\x5C___/|_| \x5C_| |_| |_____|  \x5C____/_/   \x5C_\x5C_| \x5C_\x5C_____\x5C___/";
     echo "";
     echo "          push-button installation of Geant4/Root/GATE on macOS";
     echo "";
     echo "                                  -----";
     echo "";
-
     echo "This script automates the downloading, compilation, and installation of";
     echo "Geant4, ROOT, and GATE on macOS. It is designed to be used on a vanilla";
     echo "macOS installation. Please note that it will install Xcode tools, Homebrew,";
@@ -116,7 +117,9 @@ function make_dl_dir(){
 # install xcode tools
 function install_xcode_cltools(){
     if which gcc; then echo "Xcode tools already installed"; else
+        echo "";
         echo "Installing Xcode tools..."
+        echo "";
         xcode-select --install
     fi
 }
@@ -124,7 +127,9 @@ function install_xcode_cltools(){
 # install homebrew
 function install_homebrew(){
     if which brew; then echo "Homebrew already installed"; else
-        echo "Installing Homebrew..."
+        echo "";
+        echo "Installing Homebrew...";
+        echo "";
         /usr/bin/ruby -e "$(curl -fsSL $BREW_URL)"
     fi
     if ! which brew; then brew install wget; fi
@@ -134,12 +139,16 @@ function install_homebrew(){
 function install_geant4(){
 
     # download and extract geant
+    echo "";
     echo "Downloading Geant4..."
+    echo "";
     wget $GEANT4_URL -P $DOWN_DIR
     tar -xvf $DOWN_DIR/$GEANT4_FILENAME -C $DOWN_DIR/
 
     # install required packages
-    echo "Installing Geant4 requirements..."
+    echo "";
+    echo "Installing Geant4 requirements...";
+    echo "";
     brew install cmake qt@5 xerces-c
 
     # add qt5 to path
@@ -160,7 +169,9 @@ function install_geant4(){
         -S $GEANT4_SRC_PTH -B $GEANT4_BLD_PTH
 
     # compile
+    echo "";
     echo "Compiling Geant4..."
+    echo "";
     cd $GEANT4_BLD_PTH
     make -j $N_PROC
     make install
@@ -168,7 +179,7 @@ function install_geant4(){
     # add geant4 script to path
     PWD=$(pwd)
     cd /usr/local/bin
-    source gean4.sh
+    source geant4.sh
     cd $PWD
     echo 'cd /usr/local/bin' >> ~/.zshrc
     echo 'source geant4.sh' >> ~/.zshrc
@@ -181,8 +192,11 @@ function install_root(){
 
     # install xcode
     if [ -d "/Applications/Xcode.app" ]; then
+        echo "";
         echo "Xcode already installed."
+        echo "";
     else
+        echo "";
         echo "Root requires Xcode to be installed through the native Apple Appstore.";
         echo "Xcode does not appear to be installed. The Appstore is opening now and";
         echo "directing to the Xcode download page. Please download/install. Only";
@@ -192,6 +206,7 @@ function install_root(){
         press_key
 
         # install xcode additions
+        echo "";
         echo "Xcode will require some additional items to be downloaded. Opening";
         echo "Xcode now. Please download/install. Only once this installation is";
         echo "completed, may you continue (it is okay to then close Xcode).";
@@ -201,7 +216,9 @@ function install_root(){
     fi
 
     # install root with homebrew
+    echo "";
     echo "Installing Root..."
+    echo "";
     brew install root
 
     # add root to path
@@ -211,7 +228,9 @@ function install_root(){
 
 # install gate
 function install_gate(){
+    echo "";
     echo "Installing GATE..."
+    echo "";
 }
 
 # procedure
